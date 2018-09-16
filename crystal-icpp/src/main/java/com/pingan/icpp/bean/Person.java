bac2fd557686db6178b1f2c8a90d7c840ebe55cb
liuyun <960269778@qq.com>
2018-09-16 13:21:46 +0800

    first commit

---
+++ crystal-icpp/src/main/java/com/pingan/icpp/bean/Person.java
@@ -0,0 +1,14 @@
+package com.pingan.icpp.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+	String name;
+	int age;
+}
